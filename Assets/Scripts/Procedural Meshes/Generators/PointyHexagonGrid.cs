5c6144288543d0bb65666e992bd85547a68a47c1
Mstislav Dobrotvorskiy <mstislavd@gmail.com>
2024-04-10 16:50:03 +0300

    implement (2.2) Seven Vertices

--- Assets/Scripts/Procedural Meshes/Generators/PointyHexagonGrid.cs
+++ Assets/Scripts/Procedural Meshes/Generators/PointyHexagonGrid.cs
@@ -21,11 +21,48 @@ namespace ProceduralMeshes.Generators
 
         public void Execute<S>(int z, S streams) where S : struct, IMeshStreams
         {
+            float h = sqrt(3f) / 4f;
+
             int vi = 7 * Resolution * z, ti = 6 * Resolution * z;
 
-            for (int x = 0; x < Resolution; x++, vi += 4, ti += 2)
+            for (int x = 0; x < Resolution; x++, vi += 7, ti += 6)
             {
-                
+                var xCoordinates = float2(-h, h) / Resolution;
+                var zCoordinates = float4(-0.5f, -0.25f, 0.25f, 0.5f) / Resolution; 
+
+                var vertex = new Vertex();
+                vertex.normal.y = 1f;
+                vertex.tangent.xw = float2(1f, -1f);
+
+                vertex.texCoord0 = 0.5f;
+                streams.SetVertex(vi + 0, vertex);
+
+                vertex.position.z = zCoordinates.x;
+                vertex.texCoord0.y = 0f;
+                streams.SetVertex(vi + 1, vertex);
+
+                vertex.position.x = xCoordinates.x;
+                vertex.position.z = zCoordinates.y;
+                vertex.texCoord0 = float2(0.5f - h, 0.25f);
+                streams.SetVertex(vi + 2, vertex);
+
+                vertex.position.z = zCoordinates.z;
+                vertex.texCoord0.y = 0.75f;
+                streams.SetVertex(vi + 3, vertex);
+
+                vertex.position.x = 0f;
+                vertex.position.z = zCoordinates.w;
+                vertex.texCoord0 = float2(0.5f, 1f);
+                streams.SetVertex(vi + 4, vertex);
+
+                vertex.position.x = xCoordinates.y;
+                vertex.position.z = zCoordinates.z;
+                vertex.texCoord0 = float2(0.5f + h, 0.75f);
+                streams.SetVertex(vi + 5, vertex);
+
+                vertex.position.z = zCoordinates.y;
+                vertex.texCoord0.y = 0.25f;
+                streams.SetVertex(vi + 6, vertex);
             }      
         }
     }
